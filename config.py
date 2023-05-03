5ffe974604b8d6e1150c2f23e217f4a20e1fdfd0
GhoulsBotXd2 <whiteblue075@protonmail.com>
2023-05-04 02:11:27 +0530

    Update config.py
    Signed-off-by: GhoulsBotXd2 <whiteblue075@protonmail.com>

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ START_IMG = getenv("START_IMG", "https://te.legra.ph/file/2941b97ce13e2347bac8f.
 SESSION = getenv("SESSION", None)
 
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/RealmBotSupport")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/RealmBotSupport")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/RealmBotUpdates")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
 
